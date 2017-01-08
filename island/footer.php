d0a502ca82131c3cc70d428a0472282e55b4f10c
arman-malekzade <arman.malekzade@aut.ac.ir>
2017-01-08 08:39:00 +0330

    Add files via upload

---
+++ island/footer.php
@@ -0,0 +1,8 @@
+<footer class="page-footer orange">
+
+  <div class="footer-copyright">
+    <div class="container">
+    &copy;2016 Designed by <a class="orange-text text-lighten-3" href="http://malekzade.com">Arman Malekzade</a>
+    </div>
+  </div>
+</footer>
---
+++ island/head.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+  <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0"/>
+  <title><?php echo lang_english\page_attributes::title();?></title>
+  <link rel="icon"
+        type="image/png"
+        href="images/favicon.png">
+  <!-- CSS  -->
+  <link href="css/icon.css" rel="stylesheet">
+  <link href="css/materialize.css" type="text/css" rel="stylesheet" media="screen,projection"/>
+  <link href="css/style.css" type="text/css" rel="stylesheet" media="screen,projection"/>
+  <script src="../software/javascript/annyang.js"></script>
+  <script src="../software/javascript/speechrecognition.js"></script>
+
+
+</head>
